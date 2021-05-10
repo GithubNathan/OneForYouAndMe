5cf447e57814fb5ea829e6bdc81f7c1f472aee14
NathanEdwardss <83931950+NathanEdwardss@users.noreply.github.com>
2021-05-10 10:39:51 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,19 @@
+function OneForYou() {
+
+  let name = prompt("What is your name?");
+
+  if (name == "") {
+
+    alert("One for you, one for me.");
+
+  }
+
+  else {
+
+    alert("One for " + name + ", one for me.");
+
+  }
+
+}
+
+OneForYou();
